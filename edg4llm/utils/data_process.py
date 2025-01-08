2a08a21d36d782e7995de7595ad1f08cfaebecc1
alannikos <alannikos768@outlook.com>
2025-01-08 22:10:15 +0800

    add data save's module

--- edg4llm/utils/data_process.py
+++ edg4llm/utils/data_process.py
@@ -34,3 +34,7 @@ class DataProcessor:
 
         return result
 
+def save_data_to_json(data, output_path):
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
